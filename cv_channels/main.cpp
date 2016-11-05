56b0e678f7dff994e7d2ed7bef775ae78b496db0
Parker Lusk <parkerclusk@gmail.com>
2016-11-05 16:31:00 -0600

    opencv channels and converting a 2 channel in to a 1 channel matrix

---
+++ cv_channels/main.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+
+void print(std::vector<cv::Point2f> measurements) {
+	cout << "measurements = ";
+
+	// http://stackoverflow.com/a/14374550/2392520
+	for (auto&& elem: measurements) {
+		// if the current index is needed:
+		auto i = &elem - &measurements[0];
+
+		// any code including continue, break, return
+		cout << " " << elem;
+	}
+	
+	cout << endl;
+}
+
+int main() {
+
+	std::vector<cv::Point2f> measurements;
+        measurements.push_back(cv::Point2f(4,8));
+        measurements.push_back(cv::Point2f(25,200));
+        measurements.push_back(cv::Point2f(36.5,38.5));
+
+        cout << "measurements.size() = " << measurements.size() << endl;
+        print(measurements);
+
+	cv::Mat A(measurements);
+	cout << "A.type == " << A.type() << " ; A.channels == " << A.channels() << endl;
+
+
+	// A is a 2-channel matrix (because it came from Point2f). So we need to split
+	// up the channels into separate Mat objects...
+	std::vector<cv::Mat> s;
+	cv::split(A,s);
+	cout << s[0] << endl;
+	cout << s[1] << endl;
+
+	// And now concat each of the mats into one, CV_64FC1 mat.
+	// hconcat (stack by columns); vconcat (stack by rows)
+	cv::Mat B;
+	hconcat(s, B);
+	cout << endl << endl << B << endl;
+	cout << "B.type == " << B.type() << " ; B.channels == " << B.channels() << endl;
+
+	return 0;
+}
--- cv_mat_math/cv_math.cpp
+++ cv_mat_math/cv_math.cpp
@@ -27,12 +27,49 @@ int main() {
 	std::vector<cv::Point2f> measurements;
 	measurements.push_back(cv::Point2f(4,4));
 	measurements.push_back(cv::Point2f(25,25));
-	measurements.push_back(cv::Point2f(36,36));
+	measurements.push_back(cv::Point2f(36.5,36.5));
 
 	cout << "measurements.size() = " << measurements.size() << endl;
 	print(measurements);
 
+	// focal length
 	double f = 2.6;
+
+	// Is it not working because I am starting from vector<Point2f> ?
+	cv::Mat A(2,1,CV_64FC2,cv::Scalar(1,2));
+	cout << A << endl;
+	cout << A.channels() << endl;
+
+	cv::Mat B(2,2,CV_64FC1);
+	A.convertTo(B,CV_32FC1);
+	cout << B << endl;
+	cout << B.channels() << endl << endl << endl;
+	// it still didn't work :(
+
+
+
+
+	// turn vector of points into a mat
+	cv::Mat pts_nby1c2(measurements);
+	cv::Mat tmp;
+	pts_nby1c2.convertTo(tmp, CV_64FC1, 0.5);
+
+	cout << tmp << endl;
+	cout << tmp.channels() << endl;
+
+	cout << pts_nby1c2 << endl;
+	cout << pts_nby1c2.channels() << endl;
+	cout << pts_nby1c2.rows << ", " << pts_nby1c2.cols << endl;
+
+
+	// Add a third column (the focal length) to each row
+	cv::Mat pts(measurements.size(), 1, CV_64FC3, cv::Scalar(0,0,f));
+	cout << pts << endl;
+	cout << pts.channels() << endl;
+
+	tmp.copyTo(pts(cv::Rect(0, 0, pts.cols, pts.rows)));
+	cout << pts << endl;
+
 	std::vector<cv::Point2f> ell_unit_c;
 
 	// Do some math
