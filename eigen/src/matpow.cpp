768c025f7c0c7b694f7bdccd0deaa511a655a387
Parker Lusk <parkerclusk@gmail.com>
2017-05-10 22:31:40 -0600

    eigen playground

---
+++ eigen/src/matpow.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <unsupported/Eigen/MatrixFunctions>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main() {
+
+	Matrix3d A;
+	A << 1, 2, 3,
+	     4, 5, 6,
+	     7, 8, 9;
+
+	cout << "The matrix A:" << endl;
+	cout << A << endl;
+
+	cout << "The matrix A^2:" << endl;
+	cout << A.pow(2) << endl;
+
+	return 0;
+}
