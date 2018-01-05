c53396ce1115dff22483a0d9b08a055066680cfa
Parker Lusk <parkerclusk@gmail.com>
2018-01-04 22:24:10 -0500

    working pybind11 example for Python 2.7

--- pybind/src/main.cpp
+++ pybind/src/main.cpp
@@ -6,7 +6,7 @@ int add(int i, int j) {
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(cmake_example, m) {
+PYBIND11_MODULE(pybind_example, m) {
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
