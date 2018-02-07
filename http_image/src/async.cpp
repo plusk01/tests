bc2b9ec88a299ef305afc8224afee7e6fd3b0d0f
Parker Lusk <parkerclusk@gmail.com>
2018-02-06 20:34:52 -0700

    note about scope

--- http_image/src/async.cpp
+++ http_image/src/async.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include <fstream>
+#include <future>
 
 #include <cpr/cpr.h>
 
 int main(int argc, char** argv) {
     const std::string tileserver = "https://static.pexels.com/photos/2422/sky-earth-galaxy-universe.jpg";
 
-    auto future = cpr::GetCallback([](const cpr::Response& r) {
-        std::fstream imgout("myasyncimg.jpg", std::ios::out | std::ios::binary);
-        imgout.write(r.text.c_str(), r.text.size());
-        imgout.close();
-        std::cout << "Hello from the async past" << std::endl;
-    }, cpr::Url{tileserver});
+    std::vector<std::future<void>> futures;
+
+    {
+        auto future = cpr::GetCallback([](const cpr::Response& r) {
+            std::fstream imgout("myasyncimg.jpg", std::ios::out | std::ios::binary);
+            imgout.write(r.text.c_str(), r.text.size());
+            imgout.close();
+            std::cout << "Hello from the async past" << std::endl;
+        }, cpr::Url{tileserver});
+
+        // Note how if this line were commented out (i.e., `future` went out of scope here)
+        // then the async call would become blocking -- you'll see "past" before "present"
+        futures.push_back(std::move(future));
+    }
 
     std::cout << "Hello from the present" << std::endl;
 }
