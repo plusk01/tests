ef35ed2b697d9c6c4387583a04fa757d35d2134e
Parker Lusk <parkerclusk@gmail.com>
2018-12-01 14:48:34 -0500

    using cv.wait properly

--- threads/src/main.cpp
+++ threads/src/main.cpp
@@ -7,7 +7,8 @@
 #include <iostream>           
 #include <thread>             
 #include <mutex>              
-#include <condition_variable> 
+#include <condition_variable>
+#include <chrono>
 
 std::mutex mtx;
 std::condition_variable cv;
@@ -15,9 +16,9 @@ std::condition_variable cv;
 int meal = 0;
 
 /* Consumer */
-void waiter(int ordernumber){
+void waiter(int ordernumber) {
   std::unique_lock<std::mutex> lck(mtx);
-  while(meal == 0) cv.wait(lck);
+  cv.wait(lck, [](){ return meal != 0; });
   std::cout << "Order: ";
   std::cout << ordernumber + 1 << " being taken care of with ";
   std::cout << meal - 1 << " meals also ready." << std::endl;
@@ -25,22 +26,28 @@ void waiter(int ordernumber){
 }
 
 /* Producer */
-void makeMeal(int ordernumber){
+void makeMeal(int ordernumber) {
   std::unique_lock<std::mutex> lck(mtx);
   meal++;
+  std::cout << "Chef: Made meal " << meal << std::endl;
   cv.notify_one();
 }
 
-int main(){
+int main() {
 
   std::thread chefs[10];
   std::thread waiters[10];
 
   /* Initialize customers and cheifs */
-  for (int order = 0; order < 10; order++){
+  for (int order = 0; order < 10; order++) {
     chefs[order] = std::thread(makeMeal, order);
     waiters[order] = std::thread(waiter, order);
   }
+
+  // for (int order = 0; order < 10; order++) {
+  //   std::this_thread::sleep_for(std::chrono::milliseconds(15));
+  //   waiters[order] = std::thread(waiter, order);
+  // }
 
   /* Join the threads to the main threads */
   for (int order = 0; order < 10; order++) {
