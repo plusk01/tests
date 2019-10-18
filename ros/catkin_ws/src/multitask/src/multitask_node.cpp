1b14126b5a01905504ae253f83e6b600e7076dc0
Parker Lusk <parkerclusk@gmail.com>
2019-10-17 21:39:35 -0400

    multitask finished

--- ros/catkin_ws/src/multitask/src/multitask_node.cpp
+++ ros/catkin_ws/src/multitask/src/multitask_node.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <ros/spinner.h>
 
 #include <std_msgs/Bool.h>
 
@@ -15,24 +17,77 @@ public:
   Multitask(const ros::NodeHandle nh, const ros::NodeHandle nhp)
   : nh_(nh), nhp_(nhp)
   {
-    // timer-based tasks
-    tim_taskA_ = nh_.createTimer(ros::Duration(0.1), &Multitask::taskACb, this);
-    tim_taskB_ = nh_.createTimer(ros::Duration(0.01), &Multitask::taskBCb, this);
-    // tim_taskC_ = nh_.createTimer(ros::Duration(0.01), &Multitask::taskCCb, this);
+
+    // Timers will be responsible for tasks A and B, which depend
+    // on the 'heavy lifting' done in the main thread. To decouple
+    // these timers from this 'heavy lifting', we create a dedicated
+    // callback queue via a node handle. Anything created using this
+    // node handle will use the specified callback queue.
+    ros::NodeHandle nhQ(nh_);
+    nhQ.setCallbackQueue(&task_queue_);
+
+    // timer-based tasks (timers have no message queue)
+    tim_taskA_ = nhQ.createTimer(ros::Duration(0.1), &Multitask::taskACb, this);
+    tim_taskB_ = nhQ.createTimer(ros::Duration(0.1), &Multitask::taskBCb, this);
+
+    // Tasks A and B are decoupled from the main thread 'heavy lifting',
+    // but they could still be blocked by each other. To prevent them
+    // from blocking each other (only themselves---see subscriber concurrency:
+    //          https://stackoverflow.com/a/48544551/2392520),
+    // we will service the task callback queue by a pool of threads.
+    // We use an AsyncSpinner instead of MultiThreadedSpinner so that
+    // we can still have a main loop in the spin() method.
+    spinner_ = std::make_unique<ros::AsyncSpinner>(2, &task_queue_);
+    // spinner_->start(); // note: don't start until we have data to process
+
+    // Note: for preemption: https://answers.ros.org/question/250331/how-to-thread-a-specific-computationally-expensive-callback
 
     sub_in_ = nh_.subscribe("in", 1, &Multitask::inCb, this);
   }
   ~Multitask() = default;
   
+  void spin()
+  {
+    ros::Rate r(5);
+    while (ros::ok()) {
+
+      if (processNewData) {
+        // stop tasks B and C
+        spinner_->stop();
+
+        // do some heavy lifting
+        ros::Duration(3).sleep();
+
+        // allow downstream tasks to continue
+        spinner_->start();
+
+        processNewData = false;
+      }
+
+      // use single-threaded spinner to service one message from
+      // the internal, global callback queue (the default one).
+      // This callback queue only contains messages from inCb.
+      // https://answers.ros.org/question/214629/multiple-callback-queues
+      ros::spinOnce();
+
+      // maintain a loop rate of (at most) r Hz
+      r.sleep();
+    }
+  }
 
 private:
   ros::NodeHandle nh_, nhp_;
+  ros::CallbackQueue task_queue_;
+  std::unique_ptr<ros::AsyncSpinner> spinner_;
   ros::Subscriber sub_in_;
-  ros::Timer tim_taskA_, tim_taskB_, tim_taskC_;
+  ros::Timer tim_taskA_, tim_taskB_;
+
+  bool processNewData = false; ///< set by incoming ros message
 
   void inCb(const std_msgs::BoolConstPtr& msg)
   {
     ROS_INFO("Got input");
+    processNewData = true;
   }
 
   // --------------------------------------------------------------------------
@@ -49,13 +104,6 @@ private:
     ROS_INFO("Task B");
   }
 
-  // --------------------------------------------------------------------------
-
-  void taskCCb(const ros::TimerEvent& event)
-  {
-    ROS_INFO("Task C");
-  }
-
 };
 
 // ============================================================================
@@ -67,6 +115,6 @@ int main(int argc, char *argv[])
   ros::NodeHandle nhtopics("");
   ros::NodeHandle nhparams("~");
   Multitask node(nhtopics, nhparams);
-  ros::spin();
+  node.spin();
   return 0;
 }
